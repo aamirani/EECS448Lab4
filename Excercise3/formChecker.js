b55848c541c88673c2ed21bcb2e9832653a22524
Armaan Amirani <armaanamirani@gmail.com>
2017-09-25 13:59:28 -0500

    Completed HTML page for excercise 3

---
+++ Excercise3/formChecker.js
@@ -0,0 +1,10 @@
+//All variables
+var username = document.getElementsByName('username');
+var password = document.getElementsByName('password');
+var gloves = document.getElementsByName('gamount');
+var shoes = document.getElementsByName('samount');
+var hats = document.getElementsByName('hamount');
+var shipping = document.getElementsByName('shipping');
+function check(){ //form check
+
+}
