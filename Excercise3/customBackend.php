f42ac4d553e8f80d0b371e59fa02ca5fd2b74d15
Armaan Amirani <armaanamirani@gmail.com>
2017-09-29 14:50:40 -0500

    Completed table and styling of reciept and front end

--- Excercise3/customBackend.php
+++ Excercise3/customBackend.php
@@ -1,4 +1,5 @@
 <?php
+//Definining variables to use
 $gprice = $_POST['gamount'];
 $sprice = $_POST['samount'];
 $hprice = $_POST['hamount'];
@@ -11,17 +12,30 @@ elseif ($_POST['shipping'] == 'Overnight') {
 elseif($_POST['shipping'] == "3 Day"){
     $shipprice = 5;
 }
+
 $total = 30*$gprice + $sprice * 50 + $hprice * 10 + $shipprice;
+
+//begin HTML printing
+echo'<html>';
+echo'<head>';
+echo'<link rel="stylesheet" href="style.css">';
+echo'<title>Reciept</title>';
+echo'</head>';
+echo'<body>';
+
+
 echo'<div class="reciept">';
-echo'Welcome ' . $_POST['username'] . ' with password: ' . $_POST['password'];
-echo'<table>';
-echo'<tr><th></th><th>Quantity</th><th>Cost Each</th><th>Subtotal</th></tr>';
+echo'<h2></h2>Welcome ' . $_POST['username'] . ' with password: ' . $_POST['password'] . '</h2>';
+echo'<table border="1">';
+echo'<tr><th>Quantity</th><th>Cost Each</th><th>Subtotal</th></tr>';
 echo'<tr><td>Glove</td><td>$30</td><td>$' . $gprice * 30 . '</td></tr>';
 echo'<tr><td>Shoes</td><td>$50</td><td>$' . $sprice * 50 . '</td></tr>';
 echo'<tr><td>Hats</td><td>$10</td><td>$' . $hprice * 10 . '</td></tr>';
-echo'<tr><td>Shipping</td><td>' . $_POST['shipping'] . '</td></tr>';
-echo'<tr><td>Total Cost: </td><td>$' . $total . '</td></tr>';
+echo'<tr><td>Shipping</td><td>' . $_POST['shipping'] . '</td><td>$ ' . $shipprice . '</td></tr>';
+echo'<tr><td>Total Cost: </td><td></td><td>$' . $total . '</td></tr>';
 echo'</table>';
 echo'</div>';
-
+echo'</body>';
+echo'</html>';
+//end
 ?>
--- Excercise3/formChecker.js
+++ Excercise3/formChecker.js
@@ -6,23 +6,29 @@ function check(){ //form check
     var shoes = document.getElementsByName('samount')[0].value;
     var hats = document.getElementsByName('hamount')[0].value;
     var shipping = document.getElementsByName('shipping')[0].value;
-    if(password == ""){
-        alert("Password cannot be empty");
-    }
     if(username == ""){
         alert("Username cannot be empty");
+        return false;
+    }
+    if(password == ""){
+        alert("Password cannot be empty");
+        return false;
     }
     if(gloves < 0 || hats < 0 || shoes < 0){
-        alert("Amounts cannot be less than 0")
+        alert("Amounts cannot be less than 0");
+        return false;
     }
     if(gloves == "" || hats == "" || shoes == ""){
-        alert("Amounts cannot be less than 0")
+        alert("Amounts cannot be less than 0");
+        return false
     }
     if(!isEmail(username)){
         alert("Username must be in an email format");
+        return false;
     }
     if(shipping = ''){
         alert("Please pick a shipping option");
+        return false;
     }
     
 }
